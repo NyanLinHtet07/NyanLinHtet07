ce959edc5d8c792cd8fc939eb794fbb60f0fafa0
Nyan Lin Htet <nyanlinhtet206@gmail.com>
2022-07-25 23:12:16 +0630

    update_ui

--- resources/js/Charts/PieChart.vue
+++ resources/js/Charts/PieChart.vue
@@ -1,66 +1,83 @@
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+    <Pie
+               :chart-options="chartOptions"
+               :chart-data="chartData"
+               :chart-id="chartId"
+               :dataset-id-key="datasetIdKey"
+               :plugins="plugins"
+               :css-classes="cssClasses"
+               :styles="styles"
+               :width="width"
+               :height="height"
+               />
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import { Pie } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
 
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
 export default {
-  name: 'BarChart',
-  components: { Bar },
+     name: 'PieChart',
+  components: {
+    Pie
+  },
   props: {
     chartId: {
       type: String,
-      default: 'bar-chart'
+      default: 'pie-chart'
     },
-    datasetIdKey: {
-      type: String,
-      default: 'label'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {}
-    },
-    plugins: {
-      type: Object,
-      default: () => {}
-    }
+   datasetIdKey: {
+               type: String,
+               default: 'label'
+          },
+          width: {
+               type: Number,
+               default: 100
+          },
+          height: {
+               type: Number,
+               default: 100
+          },
+          cssClasses: {
+               default: '',
+               type: String
+          },
+          styles: {
+               type: Object,
+               default: () => {}
+          },
+       
+  
+
   },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-  }
+
+
+        data() {
+            return {
+            chartData: {
+                labels: [ 'January', 'February', 'March'],
+                datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: ['#41B883', '#E46651', '#00D8FF'],
+                    data: [40, 20, 12]
+                }
+                ]
+            }
+            }
+  },
+    
+
+    
 }
 </script>
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -5,18 +5,27 @@
                 Dashboard
             </h2>
         </template>
-       <pieChart class=" w-3/6 h-3/6"/>
+        <div class=" grid grid-cols-1 md:grid-cols-2 gap-3 h-2/4 mx-2">
+            <div class=" rounded px-2 py-3 bg-white/60 backdrop-blur-md">
+                <BarChart/>
+            </div>
+             <div class=" rounded px-2 py-3 bg-white/60 backdrop-blur-md">
+                <PieChart/>
+            </div>
+        </div>
+       
     </BreezeAuthenticatedLayout>
 </template>
 
 <script>
     import BreezeAuthenticatedLayout from '@/Layouts/AdminDash.vue';
-    import pieChart from '@/Charts/PieChart.vue'
+    import PieChart from '@/Charts/PieChart.vue';
+    import BarChart from '@/Charts/BarChart.vue';
     import { Head } from '@inertiajs/inertia-vue3';
 
     export default { 
         components:{
-            BreezeAuthenticatedLayout, Head, pieChart
+            BreezeAuthenticatedLayout, Head, BarChart, PieChart
         }
     }
 </script>
